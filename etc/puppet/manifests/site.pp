5394f690e083bfc1e53d0dbcb68d04c37930d21d
Skoobiekins-draugr <scub@darchoods.net>
2014-02-22 22:06:51 -0800

    Adding stub puppet site definition for production environment, currently only bootstrapping takes place

---
+++ etc/puppet/manifests/site.pp
@@ -0,0 +1,26 @@
+import 'bootstrap'
+#
+#    Bootstrap base box with minimal host
+# information to boot on.
+#
+node 'ubuntu1310-i386' {
+    include bootstrap 
+
+    notify { 'Spam': message => 'Bootstrap successful, restarting guest machine for changes to take effect.' }
+    exec   {
+        'Trigger Restart':
+            command => "/sbin/shutdown -r now",
+            require => Class[ 'bootstrap' ]
+    }
+
+    Class[ 'bootstrap' ] -> Notify[ 'Spam' ] -> Exec[ 'Trigger Restart' ]
+}
+
+#   After Bootstrapping we install our dev
+# environment, and then begin to deploy our 
+# service set. [Nginx, ElasticSearch, Posgresql]
+#
+node 'spf' {
+
+    include bootstrap
+}
