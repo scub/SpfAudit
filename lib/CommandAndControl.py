3adef53726addca1a526d8886d045ba268cb080a
Skoobiekins-draugr <scub@darchoods.net>
2014-03-03 06:25:12 +0000

    Refactoring lib.CommandAndControl, integrating json broker and initialization helpers

--- lib/CommandAndControl.py
+++ lib/CommandAndControl.py
@@ -11,19 +11,17 @@ from geoip2.database      import Reader
 # Custom Libs
 from bots.bases           import LoggedBase
 from bots.dnsBroker       import dnsBroker 
+from bots.esBroker        import esBroker
 from lib.types.nameserver import NameServer
 
 class CommandAndControl( LoggedBase ):
-    """
-        Command And Control
-        -------------------
-
-            Acts as an intermediary between all processes,
-        managing queues and shared resources. 
-
-    """
     
-    def __init__( self, workerCount=3, logPath = 'var/log/gmx_snoop.log', geoipPath = None ):
+    def __init__( self, 
+                  workerCount  = 3, 
+                  eBrokerCount = 3, 
+                  sBrokerCount = 1, 
+                  logPath      = 'var/log/gmx_snoop.log', 
+                  geoipPath    = None ):
         """
             Create new CommandAndControl object.
 
@@ -37,12 +35,28 @@ class CommandAndControl( LoggedBase ):
         
         self.state = {
             'nameserver'  : NameServer(),
+
+            # Worker / Broker Lists 
+            'esBrokers'   : [],
+            'sqlBrokers'  : [],
             'workers'     : [],
-            'workerCount' : int( workerCount ),
-            'sql'         : None,
-            
+
+            # Worker / Broker Counts 
+            'workerCount' : int( workerCount  ),
+            'eBrkrCnt'    : int( eBrokerCount ),
+            'sBrkrCnt'    : int( sBrokerCount ),
+
+            # Worker Input Queue
             'qin'         : Queue(),
+            # Output Queue To Be Replaced
             'qout'        : Queue(),
+
+            # Worker Sql Broker Queue
+            'sqout'       : Queue(),
+
+            # Worker Json Broker Queue
+            'eqout'       : Queue(),
+
             'logPath'     : logPath,
 
             # QUEUE THROTTLING ( 100 * # Workers )
@@ -51,11 +65,14 @@ class CommandAndControl( LoggedBase ):
 
             # GeoIP Goodies
             'geoip'       : None if geoipPath is None else Reader( geoipPath ),
+
+            # last used id
+            'lastId'      : 0,
         }
 
-        for i in range( self.state[ 'workerCount' ] ):
-            self.addWorker( i + 1 )
-            
+        self._initWorkforce()
+
+
         self._log( 'init', 'DEBUG', 'Command and control initialized, workers queued and ready for input.' )
         
             
@@ -64,19 +81,53 @@ class CommandAndControl( LoggedBase ):
             Avoiding Cyclic References In Your Design Leads
             to destructors performing admirably.
         """
-        for queue in [ 'qin', 'qout' ]:
-            self.state[ queue ].cancel_join_thread()
-            self.state[ queue ].close()
-        
-        for worker in self.state[ 'workers' ]:
-            worker[ 'proc' ].terminate()
-            worker[ 'proc' ].join()
-            
-        self.state[ 'qin' ]
-        
+        # Stop Workers
+        for workerList in [ self.state[ i ] for i in [ 'workers', 
+                                                       'sqlBrokers', 
+                                                       'esBrokers' ] ]:
+            map( self._stopWorker, self.state[ workerList ] ) 
+
+        # Flush Queues
+        map( self._flushQueue, 
+             [ self.state[ queue ] for queue in [ 'qin',
+                                                  'qout',
+                                                  'sqout', 
+                                                  'eqout' ] ] )
+
         #print 'Nameserver statistics'
         self.state[ 'nameserver' ].stats()
     
+    def _newId( self ):
+        """
+            Return new id, and increment id counter 
+
+               We start our Id's at one to account
+            for command and control being worker 0
+
+        """
+        self.state[ 'lastId' ] += 1
+        return self.state[ 'lastId' ]
+
+    def _initWorkforce( self ):
+
+        """
+            Initialize Workers And Brokers
+        """
+
+        for ( wList, count, SpawnBot, queue, Broker ) in [ 
+            ( 'workers',    self.state[ 'workerCount' ], self.addWorker, None,                  None      ),
+            #( 'sqlBrokers', self.state[ 'eBrkrCnt'    ], self.addBroker, self.state[ 'sqout' ], sqlBroker ),
+            ( 'esBrokers',  self.state[ 'sBrkrCnt'    ], self.addBroker, self.state[ 'eqout'  ], esBroker  ) ]:
+
+            lid        = self.state[ 'lastId' ]
+            start, end = lid, count + lid
+            for i in range( start, end ):
+                if wList != 'workers':
+                    SpawnBot( self._newId(), Broker, wList, queue ) 
+                else:
+                    SpawnBot( self._newId() ) 
+
+
     def addWorker( self, worker_id ):
         """
             Spawn Worker And Add To Workers List
@@ -87,7 +138,9 @@ class CommandAndControl( LoggedBase ):
                             nameserver = self.state[ 'nameserver' ], 
                             geoip      = self.state[ 'geoip'      ], 
                             qin        = self.state[ 'qin'        ], 
-                            qout       = self.state[ 'qout'       ], 
+                            #qout       = self.state[ 'qout'       ], 
+                            sqout      = self.state[ 'sqout'      ],
+                            eqout      = self.state[ 'eqout'      ],
                             metaQin    = metaQin, 
                             metaQout   = metaQout )
                             
@@ -101,6 +154,28 @@ class CommandAndControl( LoggedBase ):
         } )
 
         #self.state[ 'workers' ][-1][ 'proc' ].start()
+
+    def addBroker( self, worker_id, Broker, brokerList, qin ):
+        """
+            Spawn Broker and add to brokers list
+        """
+        #import pdb; pdb.set_trace()
+        metaQin, metaQout = Queue(), Queue()
+
+        broker = Broker( workerId = worker_id, 
+                         logPath  = self.state[ 'logPath' ],
+                         qin      = qin,  
+                         metaQin  = metaQin,
+                         metaQout = metaQout,
+                         geoip    = self.state[ 'geoip' ] )
+
+        self.state[ brokerList ].append( {
+            'id'     : worker_id,
+            'proc'   : Process( target=broker.background ),
+            'broker' : broker,
+            'mQin'   : metaQin,
+            'mQout'  : metaQout,
+        } )
 
     def _flushQueue( self, queue ):
         """
@@ -121,21 +196,33 @@ class CommandAndControl( LoggedBase ):
         worker[ 'proc' ].terminate()
         worker[ 'proc' ].join()
 
-        map( self._flushQueue, [ worker[ 'mQin' ], worker[ 'mQout' ] ] )
+        map( self._flushQueue, [ worker[ i ] for i in [ 'mQin', 'mQout' ] ] )
             
     def powerWorkforce( self ):
         """
             Start workforce to begin processing queued 
+
         """
-        map( lambda worker: worker[ 'proc' ].start(), self.state[ 'workers' ] )
+        for workerList in [ self.state[ i ] for i in [ 'workers', 
+                                                       'sqlBrokers', 
+                                                       'esBrokers' ] ]:
+ 
+            map( lambda worker: worker[ 'proc' ].start(), workerList )
+
     
     def cleanupWorkforce( self ):
         """
-            Power Down Workforce
-        """
-        map( self._stopWorker, self.state[ 'workers' ] )
-        map( self._flushQueue, [ self.state[ 'qin' ], self.state[ 'qout' ] ] )
 
+            Power Down Workforce
+
+        """
+        for workerList in [ self.state[ i ] for i in [ 'workers', 
+                                                       'sqlBrokers', 
+                                                       'esBrokers' ] ]:
+            map( self._stopWorker, workerList )
+
+        map( self._flushQueue, 
+             [ self.state[ i ] for i in [ 'qin', 'sqout', 'eqout' ] ] )
 
     def pushTargets( self, target_generator ):
         """
@@ -182,17 +269,14 @@ class CommandAndControl( LoggedBase ):
 
             sleep( self.state[ 'qin' ].qsize() )
         
-        while stop_count < self.state[ 'workerCount' ]:
-            node = self.state[ 'qout' ].get()
+        while not self.state[ 'sqout' ].empty():
+            node = self.state[ 'sqout' ].get()
                         
-	    if type( node ) == str:
-	            if node.find( "STOP" ) != -1:
-        	        stop_count += 1
-            else:
-		if all( map( lambda x: x is not None, 
-			     [ node.url, node.a_records, node.mx_records ] ) ):
-			print node
+            if type( node ) != str:
+                if all( map( lambda x: x is not None, [ node.url, node.a_records, node.mx_records ] ) ):
+                    print node
             
         self._log( 'collect', 'DEBUG', 
             'Data harvest completed successfully! <3'
         )
+
