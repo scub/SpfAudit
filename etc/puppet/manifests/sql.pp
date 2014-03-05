e0011d1e9958002417ddf9be8332f09f46ae2105
Skoobiekins-draugr <scub@darchoods.net>
2014-03-05 01:17:14 +0000

    Extending sql manifest to include python api for postgresql (psycopg)

--- etc/puppet/manifests/sql.pp
+++ etc/puppet/manifests/sql.pp
@@ -9,6 +9,12 @@ class sql {
         'SpfAudit':
             user     => 'vagrant',
             password => postgresql_password( 'vagrant', 'ThGK4E4!UxJOElzMq5RysiG6j' );
+    } ->
+
+    # Python API
+    package {
+        [ 'python-psycopg2', 'python-psycopg2-dbg', 'python-psycopg2-doc' ]:
+            ensure => installed;
     }
 
 }
