087706bc73bb675409aecd0bbc878d5cbd77f7fd
Skoobiekins-draugr <scub@darchoods.net>
2014-03-29 16:13:45 -0700

    Extending audit case for command and control to test the pause/continue functionality in tandem with queued input

--- var/audit/test_CommandAndControl.py
+++ var/audit/test_CommandAndControl.py
@@ -9,6 +9,9 @@ from lib.CommandAndControl import CommandAndControl as BotMaster
 from lib.types.node        import Node
 
 
+import pdb
+
+
 def targeting():
     """
         Pull our benchmarking data
@@ -17,20 +20,28 @@ def targeting():
         for line in fd:
             yield Node( url = line.strip() )
 
-cnc = BotMaster( workerCount = 1, logPath = 'var/log/audit_commandAndControl.log', geoipPath = '/usr/share/geoip/GeoLite2-City.mmdb' )
+cnc = BotMaster( workerCount = 1, eBrokerCount = 1, sBrokerCount = 1, logPath = 'var/log/audit_commandAndControl.log', geoipPath = '/usr/share/geoip/GeoLite2-City.mmdb' )
 
 mQin, mQout = cnc.state[ 'workers' ][ 0 ][ 'mQin' ], cnc.state[ 'workers' ][ 0 ][ 'mQout' ]
 mQin.put( ( "VERBOSE", None ) ) 
 
-cnc.pushTargets( targeting )
+
+#cnc.pushTargets( targeting )
 cnc.powerWorkforce()
 
+# TESTING PAUSE
+pdb.set_trace()
+cnc.pauseWorkforce()
+cnc.pushTargets( targeting )
+pdb.set_trace()
+cnc.pauseWorkforce()
+
+pdb.set_trace()
 while not cnc.state[ 'qin' ].empty():
     try:
         queuedMeta = mQout.get_nowait() 
-        #print "{}:".format( queuedMeta[0] ),"\n",queuedMeta[1]
+        print "{}:".format( queuedMeta[0] ),"\n",queuedMeta[1]
     except QueueEmpty as NoMeta:
         continue
 
-cnc.collect()
 cnc.cleanupWorkforce()
