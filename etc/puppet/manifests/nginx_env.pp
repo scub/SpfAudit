993ced20c3e869194457947d61523c854aec0ac2
Skoobiekins-draugr <scub@darchoods.net>
2014-04-06 15:59:13 -0700

    Extending nginx deployment to use htpasswd file

--- etc/puppet/manifests/nginx_env.pp
+++ etc/puppet/manifests/nginx_env.pp
@@ -20,6 +20,12 @@ class nginx_env {
             path    => "/etc/nginx/conf.d/default.conf",
             source  => "/vagrant/etc/puppet/files/nginx/nginx-Kibana.conf",
             require => File[ 'Nginx-Base-Config' ];
+
+        'Nginx-Kibana-Htpasswd':
+            ensure  => file,
+            path    => "/etc/nginx/conf.d/spf.aud.it.htpasswd",
+            source  => "/vagrant/etc/puppet/files/nginx/spf.aud.it.htpasswd",
+            require => File[ 'Nginx-Kibana-Vhost' ];
     }
 
 
@@ -30,7 +36,7 @@ class nginx_env {
             name       => 'nginx',
             hasstatus  => true,
             hasrestart => true,
-            require    => File[ 'Nginx-Kibana-Vhost' ];
+            require    => File[ 'Nginx-Kibana-Htpasswd' ];
 
    } 
 } 
